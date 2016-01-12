ec3159b85a95d50cdaaf35485fc3dce6ad5c15be
Jon Smithers <jsmithe2@gmu.edu>
2016-01-12 17:28:59 -0500

    phoenix: upgrade to Phoenix 2.0

--- phoenix.js
+++ phoenix.js
@@ -1,6 +1,7 @@
 // This is my configuration for Phoenix <https://github.com/sdegutis/Phoenix>,
 // a super-lightweight OS X window manager that can be configured and
 // scripted through Javascript.
+
  
 var mNone = [],
   mCmd = ['cmd'],
@@ -12,7 +13,12 @@ var mNone = [],
 // Remembers hotkey bindings.
 var keys = [];
 function bind(key, mods, callback) {
-  keys.push(api.bind(key, mods, callback));
+  keyhandler = Phoenix.bind(key, mods, callback);
+  if (keyhandler) {
+    keys.push(keyhandler);
+  } else {
+    Phoenix.notify(key + " handler failed");
+  }
 }
  
 // ############################################################################
@@ -22,7 +28,7 @@ function bind(key, mods, callback) {
 // Modal activator
 // This hotkey enables/disables all other hotkeys.
 var active = false;
-api.bind('w', ['alt'], function() {
+var alt_w = Phoenix.bind('w', ['alt'], function() {
   if (!active) {
     enableKeys();
   } else {
@@ -32,6 +38,7 @@ api.bind('w', ['alt'], function() {
  
 // These keys end Phoenix mode.
 bind('escape', [], function() {
+  Phoenix.log('escape');
   disableKeys();
 });
 bind('return', [], function() {
@@ -48,40 +55,43 @@ bind('return', [], function() {
 bind( 'space', mNone, function() {
   Window.focusedWindow().toggleFullscreen();
 });
+
+function toast(str) {
+   var m = new Modal();
+   m.message = str;
+   m.origin = {x: 50, y: 90};
+   m.duration = 3;
+   m.show();
+}
  
 // Center window.
-bind( 'c', mNone, cycleCalls(
-  toGrid,
-  [
-    [0.22, 0.025, 0.56, 0.95],
-    [0.1, 0, 0.8, 1]
-  ]
-));
- 
-// The cursor keys together with cmd make any window occupy any
-// half of the screen.
-bind( 'right', mCmd, cycleCalls(
-  toGrid,
-  [
-    [0.5, 0, 0.5, 1], 
-    [0.75, 0, 0.25, 1]
-  ]
-));
- 
-bind( 'left', mCmd, cycleCalls(
-  toGrid,
-  [
-    [0, 0, 0.5, 1],
-    [0, 0, 0.25, 1]
-  ]
-));
- 
-bind( 'down', mCmd, function() {
-  Window.focusedWindow().toGrid(0, 0.7, 1, 0.3);
-});
- 
-bind( 'up', mCmd, function() {
-  Window.focusedWindow().toGrid(0, 0, 1, 0.3);
+bind( 'c', mNone, function() {
+    var m = new Modal();
+    m.message = "hey";
+    m.origin = {x: 50, y: 90};
+    m.duration = 3;
+    m.show();
+    //setTimeout(function() { m.close() }, 1000);
+  }
+
+  //cycleCalls(
+  //toGrid,
+  //[
+  //  [0.22, 0.025, 0.56, 0.95],
+  //  [0.1, 0, 0.8, 1]
+  //]
+);
+
+
+bind( ".", mNone, function() {
+  try {
+    var rect = Window.focusedWindow().frame();
+    rect.width = rect.width*.9;
+    Window.focusedWindow().setFrame(rect);
+  }
+  catch(e) {
+    Phoenix.notify(e);
+  }
 });
  
 // The cursor keys move the focussed window.
@@ -126,194 +136,162 @@ bind( 'down', mShift, function() {
 var focusTitle = function(title) {
   var winToFocus
   Window.allWindows().forEach(function(element, index, array) {
-    api.alert("...");
+    Phoenix.notify("...");
     if (element.title().indexOf(title) == 0) {
       if (!winToFocus) {
         winToFocus = element;
       }
     }
   });
-  api.alert("set winToFocus");
+  Phoenix.log("set winToFocus");
 
   if (winToFocus) {
-    winToFocus.focusWindow();
-    api.alert("focused");
+    winToFocus.focus();
+    Phoenix.log("focused");
   } else {
-    api.alert("No window has title '"+title+"'");
+    Phoenix.log("No window has title '"+title+"'");
   }
 }
 var cycle = function(appName) {
-  //api.alert(curWin.title());
-
-  var exclusions = new Array();
-  for (var i = 1; i < arguments.length; i++) {
-    exclusions.push(arguments[i]);
-  }
-  var app = App.findByTitle(appName);
-  if (!app) {
-    api.alert("app is null");
-    api.launch(appName);
-    return;
-  }
-
-  var appWindowsTemp = app.allWindows();
-  var appWindows = new Array();
-
-  for (var i = 0; i < appWindowsTemp.length; i++) {
-    if (appWindowsTemp[i].title()) {
-      appWindows.push(appWindowsTemp[i]);
+  try {
+    var exclusions = new Array();
+    for (var i = 1; i < arguments.length; i++) {
+      exclusions.push(arguments[i]);
     }
-  }
-
-  if (appWindows.length==0) {
-    api.alert("app has no titled windows");
-    api.launch(appName);
-    return;
-  }
-
-  var curWin = Window.focusedWindow();
-  var curWinBelongsToApp = false;
-  if (curWin) { // some window is focused
-    for (var i = 0; i < appWindows.length; i++) {
-      if (curWin.title() == appWindows[i].title()) {
-        curWinBelongsToApp = true;
-        break;
+    var app = App.get(appName);
+    if (!app) {
+      Phoenix.log("app is null");
+      var result = App.launch(appName);
+      if (result) {
+        toast('focus1');
+        result.focus();
+        toast('focus2');
       }
-    }
-  }
-
-  var index = 0;
-  if (curWinBelongsToApp) {
-    if (appWindows.length == 1) {
-      api.alert("no other windows");
+      Phoenix.log('result of launch: ' + result);
       return;
     }
 
-    index++; // do not focus already-focused window
-  }
+    var appWindowsTemp = app.windows();
+    var appWindows = new Array();
 
-  while (shouldBeExcluded(appWindows[index].title())) {
-    api.alert("exclude " + appWindows[index].title());
-    index++;
-    if (index == appWindows.length) {
-      break;
-    }
-  }
-  if (index == appWindows.length) {
-    api.alert("no real windows to switch to");
-    return;
-  }
-  api.alert('Go To "' + appWindows[index].title() +'"');
-  appWindows[index].focusWindow();
-  return;
-
-  function shouldBeExcluded(title) {
-    for (var i = 0; i < exclusions.length; i++) {
-      if (title.indexOf(exclusions[i]) == 0) {
-        return true;
+    for (var i = 0; i < appWindowsTemp.length; i++) {
+      if (appWindowsTemp[i].title()) {
+        appWindows.push(appWindowsTemp[i]);
       }
     }
-    return false;
+
+    if (appWindows.length==0) {
+      Phoenix.log("app has no titled windows");
+      Phoenix.launch(appName);
+      return;
+    }
+
+    var curWin = Window.focusedWindow();
+    var curWinBelongsToApp = false;
+    if (curWin) { // some window is focused
+      for (var i = 0; i < appWindows.length; i++) {
+        if (curWin.title() == appWindows[i].title()) {
+          curWinBelongsToApp = true;
+          break;
+        }
+      }
+    }
+
+    var index = 0;
+    if (curWinBelongsToApp) {
+      if (appWindows.length == 1) {
+        Phoenix.log("no other windows");
+        return;
+      }
+
+      index++; // do not focus already-focused window
+    }
+
+    while (shouldBeExcluded(appWindows[index].title())) {
+      Phoenix.log("exclude " + appWindows[index].title());
+      index++;
+      if (index == appWindows.length) {
+        break;
+      }
+    }
+    if (index == appWindows.length) {
+      Phoenix.log("no real windows to switch to");
+      return;
+    }
+    Phoenix.log('Go To "' + appWindows[index].title() +'"');
+    appWindows[index].focus();
+    return;
+
+    function shouldBeExcluded(title) {
+      for (var i = 0; i < exclusions.length; i++) {
+        if (title.indexOf(exclusions[i]) == 0) {
+          return true;
+        }
+      }
+      return false;
+    }
+  } catch (e) {
+    Phoenix.log(e);
   }
 }
-api.bind( 'a', ['alt'], function() { api.alert(""); cycle('Atom') });
-api.bind( 'i', ['alt'], function() { api.alert(""); cycle('Google Chrome', 'Hangouts', 'Pushbullet', 'Google Play Music') })
-api.bind( 'h', ['alt'], function() { api.alert("focus"); focusTitle('Hangouts') });
-api.bind( 'p', ['alt'], function() { api.alert("focus"); focusTitle('Pushbullet') });
-api.bind( 't', ['alt'], function() { api.alert(""); cycle('iTerm') });
-api.bind( 'o', ['alt'], function() { api.alert(""); cycle('Microsoft Outlook') });
-api.bind( 'l', ['alt'], function() { api.alert(""); cycle('Microsoft Lync') });
-api.bind( 'e', ['alt'], function() { api.alert(""); cycle('Eclipse') });
-api.bind( 'f', ['alt'], function() { api.alert(""); cycle('Finder') });
+
+var x00 = Phoenix.bind( 'a', ['alt'], function() { Phoenix.log("a"); cycle('Atom') });
+var x01 = Phoenix.bind( 'b', ['alt'], function() { Phoenix.notify(""); cycle('Brackets') });
+var x02 = Phoenix.bind( 'i', ['alt'], function() { Phoenix.notify(""); cycle('Google Chrome', 'Hangouts', 'Pushbullet', 'Google Play Music') })
+var x03 = Phoenix.bind( 'h', ['alt'], function() { Phoenix.notify("focus"); focusTitle('Hangouts') });
+var x04 = Phoenix.bind( 'p', ['alt'], function() { Phoenix.notify("focus"); focusTitle('Pushbullet') });
+var x05 = Phoenix.bind( 't', ['alt'], function() { Phoenix.notify(""); cycle('iTerm') });
+var x06 = Phoenix.bind( 'r', ['alt'], function() { Phoenix.notify(""); cycle('Rocket.Chat') });
+var x07 = Phoenix.bind( 'o', ['alt'], function() { Phoenix.notify(""); cycle('Microsoft Outlook') });
+var x08 = Phoenix.bind( 'l', ['alt'], function() { Phoenix.notify(""); cycle('Microsoft Lync') });
+var x09 = Phoenix.bind( 'm', ['alt'], function() { Phoenix.notify(""); cycle('MacVim') });
+var x10 = Phoenix.bind( 'e', ['alt'], function() { Phoenix.notify(""); cycle('Eclipse') });
+var x11 = Phoenix.bind( 'f', ['alt'], function() { Phoenix.notify(""); cycle('Finder') });
 bind( 't', mNone, function() {
-  api.alert("throwing");
+  try {
   var focusedWindow = Window.focusedWindow();
-  var nextScreen = focusedWindow.screen().nextScreen()
-  if (!nextScreen) {
-    api.alert("no other screen");
+  var otherScreen = Screen.mainScreen().next();
+  Phoenix.log(Screen.mainScreen().hash());
+  Phoenix.log(Window.focusedWindow().screen().hash());
+  if (!otherScreen) {
+    Phoenix.notify("no other screen");
   } else {
-    var screenFrame = nextScreen.frameWithoutDockOrMenu();
-    var windowFrame = focusedWindow.topLeft();
-    Window.focusedWindow().setTopLeft({x: screenFrame.x, y: screenFrame.y});
-    api.alert("threw window");
+    var screenFrame = otherScreen.visibleFrameInRectangle();
+    Phoenix.log(otherScreen.hash())
+
+    var w = Window.focusedWindow();
+    Phoenix.log("old " + w.screen().hash());
+    w.setTopLeft({x: screenFrame.x, y: screenFrame.y});
+    Phoenix.log("new " + w.screen().hash());
+
+    Phoenix.notify("threw window");
+  }
+  } catch (e) {
+    Phoenix.log(e)
   }
   disableKeys();
 });
-// api.bind( 'i', ['alt'], function() {
-//   var app = App.findByTitle('Google Chrome');
-//   if (!app) {
-//     api.launch('Google Chrome');
-//   } else {
-//     var curWin = Window.focusedWindow();
-//     var index = -1;
-//     if (app.allWindows().length==0) {
-//       api.launch('Google Chrome');
-//     }
-//     app.allWindows().forEach(function(element, index2, array) {
-//       if (element.title() == curWin.title()) {
-//         index = index2;   
-//       }
-//     });
-// 
-//     var finalIndex=index;
-//     index++;
-//     if (index == app.allWindows().length) {
-//       index = 0;
-//     }
-//     while ( ((!app.allWindows()[index].title()) && (index!=finalIndex)) || (app.allWindows()[index].title().indexOf('Hangouts')==0) ) {
-//       index++;
-//       if (index == app.allWindows().length) {
-//         index = 0;
-//       }
-//     }
-//     app.allWindows()[index].focusWindow();
-//   }
-// });
-// api.bind( 't', ['alt'], function() {
-//   var app = App.findByTitle('iTerm');
-//   if (!app || !app.firstWindow()) {
-//     api.launch('iTerm');
-//   } else {
-//     var win = app.firstWindow().focusWindow();
-//   }
-// });
-// api.bind( 'o', ['alt'], function() {
-//   var app = App.findByTitle('Microsoft Outlook');
-//   if (!app) {
-//     api.launch('Microsoft Outlook')
-//   } else {
-//     var win = app.firstWindow();
-//     win.focusWindow();
-//   }
-// });
-// api.bind( 'e', ['alt'], function() {
-//   var app = App.findByTitle('Eclipse');
-//   if (!app) {
-//     api.launch('Eclipse');
-//   } else {
-//     var win = app.firstWindow();
-//     win.focusWindow();
-//   }
-// });
-// api.bind( 'f', ['alt'], function() {
-//   var app = App.findByTitle('Finder');
-//   if (!app || !app.firstWindow()) {
-//     api.launch('Finder');
-//   } else {
-//     app.firstWindow().focusWindow();
-//   }
-// });
 bind( 'f', mNone, function() {
-  Window.focusedWindow().toFullScreen();
+  var rect = Window.focusedWindow().screen().visibleFrameInRectangle();
+  Window.focusedWindow().setFrame(rect);
   disableKeys();
 });
 bind ('l', mNone, function() {
-  Window.focusedWindow().toE();
+    var rect = Screen.mainScreen().visibleFrameInRectangle();
+    rect.width = rect.width/2;
+    rect.x = rect.width;
+    Window.focusedWindow().setFrame(rect);
   disableKeys();
 });
 bind ('h', mNone, function() {
-  Window.focusedWindow().toW();
-  disableKeys();
+  try {
+    var rect = Screen.mainScreen().visibleFrameInRectangle();
+    rect.width = rect.width/2;
+    Window.focusedWindow().setFrame(rect);
+    disableKeys();
+  } catch (e) {
+    Phoenix.log(e)
+  }
 });
  
 // Chrome Devtools
@@ -326,7 +304,7 @@ bind( 'd', mNone, function() {
   browseWindow = chrome.findWindowNotMatchingTitle('^Developer Tools -'),
   devToolsWindow = chrome.findWindowMatchingTitle('^Developer Tools -');
  
-  api.alert( 'Chrome Dev Tools Layout', 0.25 );
+  Phoenix.notify( 'Chrome Dev Tools Layout', 0.25 );
  
   if ( browseWindow ) {
   browseWindow.toE();
@@ -357,6 +335,9 @@ function cycleCalls(fn, argsList) {
   fn.apply(this, argsList[argIndex]);
   };
 }
+
+var windowModal = new Modal();
+windowModal.message = "Managing Windows";
  
 // Disables all remembered keys.
 function disableKeys() {
@@ -364,7 +345,7 @@ function disableKeys() {
   _(keys).each(function(key) {
     key.disable();
   });
-  api.alert("done", 0.5);
+  windowModal.close();
 }
  
 // Enables all remembered keys.
@@ -373,187 +354,8 @@ function enableKeys() {
   _(keys).each(function(key) {
     key.enable();
   });
-  api.alert("Phoenix", 0.5);
+  windowModal.show();
 }
- 
-// ### Helper methods `Window`
-//
-// #### Window#toGrid()
-//
-// This method can be used to push a window to a certain position and size on
-// the screen by using four floats instead of pixel sizes.  Examples:
-//
-//     // Window position: top-left; width: 25%, height: 50%
-//     someWindow.toGrid( 0, 0, 0.25, 0.5 );
-//
-//     // Window position: 30% top, 20% left; width: 50%, height: 35%
-//     someWindow.toGrid( 0.3, 0.2, 0.5, 0.35 );
-//
-// The window will be automatically focussed.  Returns the window instance.
-function windowToGrid(window, x, y, width, height) {
-  var screen = window.screen().frameWithoutDockOrMenu();
- 
-  window.setFrame({
-  x: Math.round( x * screen.width ) + padding + screen.x,
-  y: Math.round( y * screen.height ) + padding + screen.y,
-  width: Math.round( width * screen.width ) - ( 2 * padding ),
-  height: Math.round( height * screen.height ) - ( 2 * padding )
-  });
- 
-  window.focusWindow();
- 
-  return window;
-}
- 
-function toGrid(x, y, width, height) {
-  windowToGrid(Window.focusedWindow(), x, y, width, height);
-}
- 
-Window.prototype.toGrid = function(x, y, width, height) {
-  windowToGrid(this, x, y, width, height);
-};
- 
-// Convenience method, doing exactly what it says.  Returns the window
-// instance.
-Window.prototype.toFullScreen = function() {
-  return this.toGrid( 0, 0, 1, 1 );
-};
- 
- 
-// Convenience method, pushing the window to the top half of the screen.
-// Returns the window instance.
-Window.prototype.toN = function() {
-  return this.toGrid( 0, 0, 1, 0.5 );
-};
- 
-// Convenience method, pushing the window to the right half of the screen.
-// Returns the window instance.
-Window.prototype.toE = function() {
-  return this.toGrid( 0.5, 0, 0.5, 1 );
-};
- 
-// Convenience method, pushing the window to the bottom half of the screen.
-// Returns the window instance.
-Window.prototype.toS = function() {
-  return this.toGrid( 0, 0.5, 1, 0.5 );
-};
- 
-// Convenience method, pushing the window to the left half of the screen.
-// Returns the window instance.
-Window.prototype.toW = function() {
-  return this.toGrid( 0, 0, 0.5, 1 );
-};
- 
- 
-// Stores the window position and size, then makes the window full screen.
-// Should the window be full screen already, its original position and size
-// is restored.  Returns the window instance.
-Window.prototype.toggleFullscreen = function() {
-  if ( previousSizes[ this ] ) {
-  this.setFrame( previousSizes[ this ] );
-  delete previousSizes[ this ];
-  }
-  else {
-  previousSizes[ this ] = this.frame();
-  this.toFullScreen();
-  }
- 
-  return this;
-};
- 
-// Move the currently focussed window left by [`nudgePixel`] pixels.
-Window.prototype.nudgeLeft = function( factor ) {
-  var win = this,
-  frame = win.frame(),
-  pixels = nudgePixels * ( factor || 1 );
- 
-  if (frame.x >= pixels) {
-  frame.x -= pixels;
-  } else {
-  frame.x = 0;
-  }
-  win.setFrame( frame );
-};
- 
-// Move the currently focussed window right by [`nudgePixel`] pixels.
-Window.prototype.nudgeRight = function( factor ) {
-  var win = this,
-  frame = win.frame(),
-  maxLeft = win.screen().frameIncludingDockAndMenu().width - frame.width,
-  pixels = nudgePixels * ( factor || 1 );
- 
-  if (frame.x < maxLeft - pixels) {
-  frame.x += pixels;
-  } else {
-  frame.x = maxLeft;
-  }
-  win.setFrame( frame );
-};
- 
-// Move the currently focussed window left by [`nudgePixel`] pixels.
-Window.prototype.nudgeUp = function( factor ) {
-  var win = this,
-  frame = win.frame(),
-  pixels = nudgePixels * ( factor || 1 );
- 
-  if (frame.y >= pixels) {
-  frame.y -= pixels;
-  } else {
-  frame.y = 0;
-  }
-  win.setFrame( frame );
-};
- 
-// Move the currently focussed window right by [`nudgePixel`] pixels.
-Window.prototype.nudgeDown = function( factor ) {
-  var win = this,
-  frame = win.frame(),
-  maxTop = win.screen().frameIncludingDockAndMenu().height - frame.height,
-  pixels = nudgePixels * ( factor || 1 );
- 
-  if (frame.y < maxTop - pixels) {
-  frame.y += pixels;
-  } else {
-  frame.y = maxTop;
-  }
-  win.setFrame( frame );
-};
- 
-// #### Functions for growing / shrinking the focussed window.
- 
-Window.prototype.growWidth = function() {
-  this.nudgeLeft(3);
- 
-  var win = this,
-  frame = win.frame(),
-  screenFrame = win.screen().frameIncludingDockAndMenu(),
-  pixels = nudgePixels * 6;
- 
-  if (frame.width < screenFrame.width - pixels) {
-  frame.width += pixels;
-  } else {
-  frame.width = screenFrame.width;
-  }
- 
-  win.setFrame(frame);
-};
- 
-Window.prototype.growHeight = function() {
-  this.nudgeUp(3);
- 
-  var win = this,
-  frame = win.frame(),
-  screenFrame = win.screen().frameIncludingDockAndMenu(),
-  pixels = nudgePixels * 6;
- 
-  if (frame.height < screenFrame.height - pixels) {
-  frame.height += pixels;
-  } else {
-  frame.height = screenFrame.height;
-  }
- 
-  win.setFrame(frame);
-};
  
 Window.prototype.shrinkWidth = function() {
   var win = this,
