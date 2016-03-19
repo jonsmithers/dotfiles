f1a67d57fa1fd766a9b32b94cc8023609b128106
Jon Smithers <jsmithe2@gmu.edu>
2016-03-19 12:55:50 -0400

    Help script to setup vim on installation

---
+++ vim/vim_setup.sh
@@ -0,0 +1,8 @@
+# Install Vim-Plug
+
+curl -fLo ~/.vim/autoload/plug.vim --create-dirs \
+    https://raw.githubusercontent.com/junegunn/vim-plug/master/plug.vim
+
+# Install Vundle
+
+git clone https://github.com/VundleVim/Vundle.vim.git ~/.vim/bundle/Vundle.vim
