9a3b7dc4a970d2ed9be0fdf6dd5679237a6c4976
Jon Smithers <jsmithe2@gmu.edu>
2016-07-01 14:56:55 -0400

    git: setup script always has right working directory

--- git/git_setup.sh
+++ git/git_setup.sh
@@ -1,4 +1,8 @@
+#!/bin/bash
+cd "$(dirname "$0")"
+
 git submodule init
 git submodule update
+
 ln -s `pwd`/diff-highlight /usr/local/bin/diff-highlight
 ln -s `pwd`/git-vim/git-vim /usr/local/bin/git-vim
