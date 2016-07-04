c13faa6b0918cba4230692762dcec5c0729d3672
Jon Smithers <jsmithe2@gmu.edu>
2016-07-03 21:08:19 -0400

    vim: update setup

--- vim/setup-vim.sh
+++ vim/setup-vim.sh
@@ -1,18 +1,20 @@
 #!/bin/bash
 cd "$(dirname "$0")"
 
+mkdir -p ~/.config/nvim
+
 # Install Vim-Plug
 
 curl -fLo ~/.vim/autoload/plug.vim --create-dirs \
     https://raw.githubusercontent.com/junegunn/vim-plug/master/plug.vim
 
-# Install Vundle
-
-git clone https://github.com/VundleVim/Vundle.vim.git ~/.vim/bundle/Vundle.vim
+curl -fLo ~/.config/nvim/autoload/plug.vim --create-dirs \
+    https://raw.githubusercontent.com/junegunn/vim-plug/master/plug.vim
 
 # Install solarized colorscheme
 
 curl --create-dirs https://raw.githubusercontent.com/altercation/vim-colors-solarized/master/colors/solarized.vim -o ~/.vim/colors/solarized.vim
+curl --create-dirs https://raw.githubusercontent.com/altercation/vim-colors-solarized/master/colors/solarized.vim -o ~/.config/nvim/colors/solarized.vim
 
 # Install Powerline fonts
 
