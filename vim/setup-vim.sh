6a740c557f50a3d83c65388b322456ae22ee50bc
Jon Smithers <jsmithe2@gmu.edu>
2016-07-05 10:31:09 -0400

    nvim: add independent fzf installation

--- vim/setup-vim.sh
+++ vim/setup-vim.sh
@@ -13,6 +13,10 @@ curl -fLo ~/.vim/autoload/plug.vim --create-dirs \
 curl -fLo ~/.config/nvim/autoload/plug.vim --create-dirs \
     https://raw.githubusercontent.com/junegunn/vim-plug/master/plug.vim
 
+# Install FZF
+git clone --depth 1 https://github.com/junegunn/fzf.git ~/.fzf
+~/.fzf/install
+
 # Install solarized colorscheme
 
 curl --create-dirs https://raw.githubusercontent.com/altercation/vim-colors-solarized/master/colors/solarized.vim -o ~/.vim/colors/solarized.vim
