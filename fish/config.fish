cc8657680be787e056f32423dfc071ad59623ff3
Jon Smithers <jsmithe2@gmu.edu>
2016-08-07 11:42:32 -0400

    fish

---
+++ fish/config.fish
@@ -0,0 +1,4 @@
+source $HOME/.config/fish/functions/fish_user_key_bindings.fish
+source $HOME/.config/fish/scratch_rc
+
+test -e {$HOME}/.iterm2_shell_integration.fish ; and source {$HOME}/.iterm2_shell_integration.fish
