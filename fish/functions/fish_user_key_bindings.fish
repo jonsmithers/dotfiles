e57b1df9f7e956f7536f21616cf02d48d7a138a0
Jon Smithers <jsmithe2@gmu.edu>
2016-08-17 11:39:02 -0400

    fish: !! and !$ now work in vi mode

--- fish/functions/fish_user_key_bindings.fish
+++ fish/functions/fish_user_key_bindings.fish
@@ -2,6 +2,8 @@ function fish_user_key_bindings
 
     bind ! bind_bang
     bind '$' bind_dollar
+    bind -M insert ! bind_bang
+    bind -M insert '$' bind_dollar
 
     ### fzf ###
     bind \ct '__fzf_ctrl_t'
