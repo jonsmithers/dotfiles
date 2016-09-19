4a8900ca39b3646cd46c686060372427015cda24
Jon Smithers <mail@jonsmithers.link>
2016-09-18 21:30:12 -0400

    git: tweak setup

--- git/setup-git.sh
+++ git/setup-git.sh
@@ -4,14 +4,17 @@ cd "$(dirname "$0")"
 git submodule init
 git submodule update
 
-ln -s `pwd`/diff-highlight /usr/local/bin/diff-highlight
+if [ ! -d "~/bin/" ]; then
+  mkdir ~/bin
+fi
+
+ln -s `pwd`/diff-highlight ~/bin/diff-highlight
 
 if [ "$(uname)" == "Darwin" ]; then
   echo Installing git-number for Mac
   brew install git-number
 elif [ "$(expr substr $(uname -s) 1 5)" == "Linux" ]; then
   echo Installing git-number for linux
-  mkdir ~/bin
   curl https://raw.githubusercontent.com/holygeek/git-number/master/git-id     > ~/bin/git-id
   curl https://raw.githubusercontent.com/holygeek/git-number/master/git-list   > ~/bin/git-list
   curl https://raw.githubusercontent.com/holygeek/git-number/master/git-number > ~/bin/git-number
