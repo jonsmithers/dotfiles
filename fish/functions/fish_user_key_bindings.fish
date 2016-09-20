2f4db1211f08323913c1cc6dcbefead5209c54af
Jon Smithers <jsmithe2@gmu.edu>
2016-09-20 10:49:06 -0400

    fish: set !$ !! key bindings after vi/fzf bindings

--- fish/functions/fish_user_key_bindings.fish
+++ fish/functions/fish_user_key_bindings.fish
@@ -1,12 +1,12 @@
 function fish_user_key_bindings
 
+    fish_vi_key_bindings
+    fzf_key_bindings
+
     bind ! bind_bang
     bind '$' bind_dollar
     bind -M insert ! bind_bang
     bind -M insert '$' bind_dollar
-
-    fish_vi_key_bindings
-    fzf_key_bindings
 
 end
 
