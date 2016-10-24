0baf3a071e202a06e6eb7f358044839eabb831e9
Jon Smithers <mail@jonsmithers.link>
2016-10-23 20:11:24 -0400

    cleanup setup file

--- vim/setup-vim.sh
+++ vim/setup-vim.sh
@@ -1,36 +1,31 @@
 #!/bin/bash
 cd "$(dirname "$0")"
 
+echo "Creating symbolic link for NeoVim"
 mkdir -p ~/.vim
 mkdir -p ${XDG_CONFIG_HOME:=$HOME/.config}
 ln -s ~/.vim $XDG_CONFIG_HOME/nvim
 
 # Install Vim-Plug
-
+echo "Installing Vim-Plug"
 curl -fLo ~/.vim/autoload/plug.vim --create-dirs \
     https://raw.githubusercontent.com/junegunn/vim-plug/master/plug.vim
-
 curl -fLo ~/.config/nvim/autoload/plug.vim --create-dirs \
     https://raw.githubusercontent.com/junegunn/vim-plug/master/plug.vim
 
 # Install FZF
+echo "Installing FZF"
 git clone --depth 1 https://github.com/junegunn/fzf.git ~/.fzf
 ~/.fzf/install
 ln -s ~/.fzf/bin/fzf /usr/local/bin/fzf
 ln -s ~/.fzf/bin/fzf-tmux /usr/local/bin/fzf-tmux
 
-# Install solarized colorscheme
-
-curl --create-dirs https://raw.githubusercontent.com/altercation/vim-colors-solarized/master/colors/solarized.vim -o ~/.vim/colors/solarized.vim
-curl --create-dirs https://raw.githubusercontent.com/altercation/vim-colors-solarized/master/colors/solarized.vim -o ~/.config/nvim/colors/solarized.vim
-
 # Install Powerline fonts
-
+echo "Installing Powerline fonts"
 git submodule init
 git submodule update
 powerline-fonts/install.sh
 
-# Tern Plugin Setup
 echo ""
 echo "To install Plug pluggins"
 echo "    :PlugInstall (inside vim)"
