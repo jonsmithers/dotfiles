abc25ccf4782620b189a8d909ac31d84265ffb24
Jon Smithers <mail@jonsmithers.link>
2016-11-10 11:01:08 -0500

    fish: Update broke my key bindings

--- fish/functions/fish_user_key_bindings.fish
+++ fish/functions/fish_user_key_bindings.fish
@@ -1,6 +1,12 @@
 function fish_user_key_bindings
 
-    fish_vi_key_bindings
+    # hybrid vim and emacs bindings
+    # https://github.com/fish-shell/fish-shell/pull/3068
+    for mode in default insert visual
+        fish_default_key_bindings -M $mode
+    end
+    fish_vi_key_bindings --no-erase
+
     fzf_key_bindings
 
     bind ! bind_bang
