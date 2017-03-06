f316efc4d291f5d12e0c8a9fa4e88da789104879
Jon Smithers <mail@jonsmithers.link>
2017-03-06 17:01:58 -0500

    fish: touchup background task starters

--- fish/config.fish
+++ fish/config.fish
@@ -260,8 +260,11 @@ set fish_greeting ""
     screen -d -m -S http http-server -p 8000 -c-1
   end
   function startairstat
-    screen -d -m -S geo fish -c 'as; and rvm default; and buildr geoserver'
-    screen -d -m -S mq  fish -c 'as; and rvm default; and buildr activemq'
+    screen -d -m -S geo fish -c 'ja; and rvm default; and buildr geoserver'
+    screen -d -m -S mq  fish -c 'ja; and rvm default; and buildr activemq'
+  end
+  function startalacritty
+      screen -d -m -S alac ~/git/alacritty/target/release/alacritty
   end
 
 #FZF
