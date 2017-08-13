aee6d496d73b1d106cbd892d545ee7763c957d7f
Jon Smithers <mail@jonsmithers.link>
2017-08-13 13:24:41 -0400

    vim: remove unnecessary setup steps taken care of by vimrc

--- vim/setup-vim.sh
+++ vim/setup-vim.sh
@@ -8,19 +8,19 @@ ln -s ~/.vim $XDG_CONFIG_HOME/nvim
 # I HAVENT REALLY IRONED THIS OUT WITH DOTPHILE SYMLINKS
 ln -s ~/.vimrc ~/.config/nvim/init.vim
 
-# Install Vim-Plug
-echo "Installing Vim-Plug"
-curl -fLo ~/.vim/autoload/plug.vim --create-dirs \
-    https://raw.githubusercontent.com/junegunn/vim-plug/master/plug.vim
-curl -fLo ~/.config/nvim/autoload/plug.vim --create-dirs \
-    https://raw.githubusercontent.com/junegunn/vim-plug/master/plug.vim
-
-# Install FZF
-echo "Installing FZF"
-git clone --depth 1 https://github.com/junegunn/fzf.git ~/.fzf
-~/.fzf/install
-sudo ln -s ~/.fzf/bin/fzf /usr/local/bin/fzf
-sudo ln -s ~/.fzf/bin/fzf-tmux /usr/local/bin/fzf-tmux
+# My vimrc should install vim-plug and fzf automatically
+# # Install Vim-Plug
+# echo "Installing Vim-Plug"
+# curl -fLo ~/.vim/autoload/plug.vim --create-dirs \
+#     https://raw.githubusercontent.com/junegunn/vim-plug/master/plug.vim
+# curl -fLo ~/.config/nvim/autoload/plug.vim --create-dirs \
+#     https://raw.githubusercontent.com/junegunn/vim-plug/master/plug.vim
+# # Install FZF
+# echo "Installing FZF"
+# git clone --depth 1 https://github.com/junegunn/fzf.git ~/.fzf
+# ~/.fzf/install
+# sudo ln -s ~/.fzf/bin/fzf /usr/local/bin/fzf
+# sudo ln -s ~/.fzf/bin/fzf-tmux /usr/local/bin/fzf-tmux
 
 # Install Powerline fonts
 echo "Installing Powerline fonts"
