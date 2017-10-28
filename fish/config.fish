80e3906f58bf1bd18beb8c3eb92f9bf30b3edb33
Jon Smithers <mail@jonsmithers.link>
2017-10-28 10:19:00 -0400

    fish: linuxbrew doctor's bidding

--- fish/config.fish
+++ fish/config.fish
@@ -104,7 +104,9 @@ set fish_greeting ""
       #alias journal="guake -t; gnome-terminal --command='wordsafe j' --full-screen --hide-menubar"
       export TMPDIR="/tmp/" # not sure why this suddenly became necessary
       set PATH /usr/local/go/bin $PATH
+      set PATH /home/smithers/.linuxbrew/sbin $PATH
       set PATH /home/smithers/.linuxbrew/bin $PATH
+      set XDG_DATA_DIRS /home/smithers/.linuxbrew/share:$XDG_DATA_DIRS
     case zamperini3
       init_echo "zamperini3 init"
       alias dsk='jump dsk'
