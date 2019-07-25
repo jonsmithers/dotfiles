f3e90e81e1a32a70eac11b9a1399a9c07e8c0304
Jon Smithers <mail@jonsmithers.link>
2019-07-25 12:20:10 -0400

    fish: cleanup

--- fish/config.fish
+++ fish/config.fish
@@ -6,53 +6,24 @@ function fish_hybrid_bindings
 end
 set -g fish_key_bindings fish_hybrid_bindings
 
-export NOTI_PUSHBULLET_ACCESSTOKEN=o.5RKGbICfF91y9S6zifyMPs6YTLdM0tdp
-
 function init_echo
   # echo $argv
 end
 
-function fish_title
-    # user can set "use_title" to override default title
-    set resulting_title $use_title $_
-    echo $resulting_title[1]
-end
-
 set fish_greeting ""
-# function fish_greeting
-#  set -l cows_dir /usr/local/Cellar/cowsay/3.04/share/cows
-#        set -l perms (ls $cows_dir/*.cow | gshuf -n1)
-#        echo $perms
-#  set -l avatar (ls $cows_dir | gshuf -n1|cut -d'.' -f1)
-#  echo $avatar
-#  cowsay -f $avatar 'Le chat miaule, what should I do?'
-#end
 
-# temporary experimental stuff
-  function disable_vi_mode
-    set -g fish_key_bindings fish_default_key_bindings
-  end
-  function powerline
-    function fish_prompt
-      ~/git/powerline-shell/powerline-shell.py $status --shell bare ^/dev/null --cwd-max-depth 1 --cwd-mode dironly --mode patched
-    end
-  end
-
-  function fish_mode_prompt # erase this function because I want to display mode in right prompt
-  end
-
-# Filesystem navigation
+# simple filesystem navigation
   # http://jeroenjanssens.com/2013/08/16/quickly-navigate-your-filesystem-from-the-command-line.html
-  export MARKPATH=$HOME/.marks
-  function jump
-    cd "$MARKPATH/$argv"; or echo No such mark $argv;
-  end
-  function mark
-      mkdir -p "$MARKPATH"; ln -s (pwd) "$MARKPATH/$argv";
-  end
-  function unmark
-      rm -i "$MARKPATH/$argv[1]"
-  end
+set MARKPATH $HOME/.marks
+function jump
+  cd "$MARKPATH/$argv"; or echo No such mark $argv;
+end
+function mark
+  mkdir -p "$MARKPATH"; ln -s (pwd) "$MARKPATH/$argv";
+end
+function unmark
+  rm -i "$MARKPATH/$argv[1]"
+end
 
   switch (uname)
     case Darwin
@@ -60,8 +31,6 @@ set fish_greeting ""
       function marks
         ls -l "$MARKPATH" | tail -n +2 | sed 's/  / /g' | cut -d' ' -f9- | awk -F ' -> ' '{printf "%-10s -> %s\n", $1, $2}'
       end
-      alias dropthebass="osascript -e 'set volume 10'"
-      alias stfu="osascript -e 'set volume output muted true'"
       alias chrome="/Applications/Google\ Chrome.app/Contents/MacOS/Google\ Chrome"
       alias tmux="env TERM=screen-256color-bce tmux" # for vim inside tmux https://stackoverflow.com/questions/10158508/lose-vim-colorscheme-in-tmux-mode
       set TERM 'xterm-256color'
@@ -173,37 +142,6 @@ set fish_greeting ""
       setjavaversion 8
       export CLICOLOR=1
       export LSCOLORS="Gxfxcxdxbxegedabagacad"
-      alias ja='jump a'
-      alias jab='jump ab'
-      alias jaj='jump aj'
-      alias jajb='jump ajb'
-      alias ju='jump uc2'
-      alias juj='jump uc2js'
-      alias asma='jump asma'
-      alias uc2='jump uc2'
-      alias leaf='jump leaf'
-      alias tomcat='~/programs/runBranch.sh'
-      alias debugmeteor="env NODE_OPTIONS='--debug' meteor run"
-      alias slatetail='tail -f /var/log/system.log | grep --line-buffered "Slate" | sed "s/.*.local Slate\[[0-9]*\]:/> /"'
-      alias wfs='/Users/smithers/git/UC2/uc2-app/support/scripts/waitForServer.js'
-      alias bumble='mount_smbfs -N //guest:@(cat ~/.config/.ip_bumble)/FileShare ~/FileShare'
-
-      function uc2b
-        echo (wfs; and ~/bin/browseUc2) &
-      end
-
-      function copyIP
-        ifconfig | grep 192 | sed -E 's/.*inet ([0-9.]+).*/http:\/\/\1:7770\/dist\//' | pbcopy
-        echo 'copied '(pbpaste)
-      end
-
-      function killMicrosoft
-        kill (ps -ef | grep Microsoft\ Database | tr -s ' ' | cut -d' ' -f 3)
-        kill (ps -ef | grep Microsoft\ Outlook | tr -s ' ' | cut -d' ' -f 3)
-        kill (ps -ef | grep Microsoft\ Alerts | tr -s ' ' | cut -d' ' -f 3)
-        kill (ps -ef | grep Microsoft\ AU | tr -s ' ' | cut -d' ' -f 3)
-        open "https://qpm.leidos.com/"
-      end
 
       function sfailp
         echo -e "\e[0;31muh oh!"
@@ -252,33 +190,20 @@ end
 # global aliases
   alias g='git number'
   alias gn='git number'
-  alias gim='g -c vim'
+  if command -v gvim > /dev/null
+    alias gim='g -c gvim -v'
+  else
+    alias gim='g -c vim'
+  end
   alias deletemergedbranches='git branch --merged | grep -v "\*" | grep -v \' master$\' | grep -v \' dev$\' | xargs -n 1 git branch -d'
   alias installgitcommithook='curl https://raw.githubusercontent.com/jonsmithers/git-commit-prepender/dev/prepare-commit-msg > ./.git/hooks/prepare-commit-msg'
-  alias gatom='g -c atom'
-  alias todos='agl "(TODO|todo).*(([sS](mithers|MITHERS))|JJS|jjs)" -A 2 -B 2'
-  alias todosgrep='git grep -I --ignore-case -E "todo.*(smithers|JJS)"'
   alias doge='echo "DOGE HERE. MUCH BASH. SUCH TERMINAL"'
-  alias shortgrep='grep --invert-match -E ".{200}"'
   alias ll='ls -lhAS'
 
-  alias dsk='jump dsk'
-  alias dls='jump dls'
-
-  alias agl='ag --pager="less -R"'
-  alias grep='grep --color=auto -I'
-  #                              ^ ignore binary files
-  #                  ^ highlight matches
-  alias igrep='grep --invert-match'
   alias fzfp='fzf --preview "head --lines=40 {}"'
 
   function giff
     g -c vim $argv[1] +Gvdiff
-  end
-
-  function howtousexargs
-    echo "ls | tr '\n' '\0' | xargs -0 -n1 -p"
-    echo "ls | sed '/s/.*/\"&\"/'"
   end
 
   # use autojump if available
@@ -293,6 +218,9 @@ end
 
   if command -v lpass > /dev/null
     function laspas
+      if not lpass status
+        lpass login spambox.js@gmail.com
+      end
       lpass show -c --password (lpass ls | fzf | awk '{print $(NF)}' | sed 's/\]//g')
     end
   end
@@ -352,66 +280,12 @@ USAGE:
     echo "    git remote add smithers git://[IP-ADDRESS]/"
     git daemon --export-all --base-path=.
   end
-  # screen -RR to attach to most recent session
-  function starttourd
-    jt
-    screen -d -m -S t gulp serve
-    jl
-    screen -d -m -S l nodemon server-index.js
-    jm
-    screen -d -m -S m nodemon
-    jt
-  end
-  function startuc2
-    uc2js
-    screen -d -m -S http http-server -p 8000 -c-1
-  end
-  function startalacritty
-    screen -d -m -S alac ~/git/alacritty/target/release/alacritty
-  end
-
-#FZF
 
   if command -v rg > /dev/null
     set -x FZF_DEFAULT_COMMAND 'rg -g "!dist" -g "!jmeter" -g "!*min.js" --files'
     set -x FZF_CTRL_T_COMMAND 'rg --files'
   else
     echo "(consider installing rg)"
-  end
-
-  function fzfhelp
-  echo ' fdr    - cd to selected parent directory'
-  echo ' cdf    - cd into the directory of the selected file'
-  echo ' fstash - manage git stash. enter to see contents, C-d to diff against HEAD, C-b to checkout as branch'
-  echo ' fshow  - git commit browser'
-  echo ' fco    - checkout branch or tag'
-  echo ' fbr    - fuzzy find branch'
-  end
-
-  function fco
-    # If we assign results to a local var, newlines are converted to spaces.
-    # That's no good. Instead we store in a local file.
-
-    git branch --all | grep -v HEAD | sed "s/.* //" | sed "s#remotes/[^/]*/##" | sort -u | fzf > /tmp/fzf.result;
-    echo  git checkout (cat /tmp/fzf.result)
-    spin "git checkout (cat /tmp/fzf.result)"
-
-    # TODO: use tmp file instead of local var for the following code
-    # set -l tags branches target
-    # set tags (git tag | awk '{print "\x1b[31;1mtag\x1b[m\t" $1}'); or return 1
-    # set branches (git branch --all | grep -v HEAD | sed "s/.* //" | sed "s#remotes/[^/]*/##" | sort -u | awk '{print "\x1b[34;1mbranch\x1b[m\t" $1}' ) # | \
-    # set target (
-    #   (echo $tags; echo $branches) | \
-    #   fzf-tmux -l30 -- --no-hscroll --ansi +m -d "\t" -n 2); or return
-    # git checkout (echo "$target" | awk '{print $2}')
-  end
-
-  function cpbr
-    # If we assign results to a local var, newlines are converted to spaces.
-    # That's no good. Instead we store in a local file.
-
-    git branch --all | grep -v HEAD | sed "s/.* //" | sed "s#remotes/[^/]*/##" | sort -u | fzf > /tmp/fzf.result;
-    echo (cat /tmp/fzf.result) | pbcopy
   end
 
   function encrypt
