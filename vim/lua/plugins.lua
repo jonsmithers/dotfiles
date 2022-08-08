ebcc6c6d31990a5f1ab950765db1657b35c8e9b5
Jon Smithers <jon@smithers.dev>
2022-08-07 21:52:59 -0400

    nvim: add treesitter context
    Because vscode is implementing this feature and I want it.

--- vim/lua/plugins.lua
+++ vim/lua/plugins.lua
@@ -400,6 +400,10 @@ packer.startup(function(use)
 
   use { -- nvim-treesitter/nvim-treesitter
     'nvim-treesitter/nvim-treesitter',
+    requires = {
+      'nvim-treesitter/nvim-treesitter-context',
+      -- TSContextEnable, TSContextDisable, TSContextToggle
+    },
     config = function()
       require'nvim-treesitter.configs'.setup {
         icons=(not dev_icons_enabled and {
@@ -429,10 +433,58 @@ packer.startup(function(use)
         special_files={},
         ensure_installed = {
           "json", "http", -- required for rest-nvim
+          "javascript", "tsx", "typescript", -- not sure if these overlap/conflict
           "lua",
           "vim",
+          "yaml",
         },
       }
+      require'treesitter-context'.setup {
+        enable = true, -- Enable this plugin (Can be enabled/disabled later via commands)
+        max_lines = 0, -- How many lines the window should span. Values <= 0 mean no limit.
+        trim_scope = 'outer', -- Which context lines to discard if `max_lines` is exceeded. Choices: 'inner', 'outer'
+        patterns = { -- Match patterns for TS nodes. These get wrapped to match at word boundaries.
+          -- For all filetypes
+          -- Note that setting an entry here replaces all other patterns for this entry.
+          -- By setting the 'default' entry below, you can control which nodes you want to
+          -- appear in the context window.
+          default = {
+            'class',
+            'function',
+            'method',
+            -- 'for', -- These won't appear in the context
+            -- 'while',
+            -- 'if',
+            -- 'switch',
+            -- 'case',
+          },
+          yaml = {
+            'block_mapping_pair'
+          },
+          -- Example for a specific filetype.
+          -- If a pattern is missing, *open a PR* so everyone can benefit.
+          --   rust = {
+          --       'impl_item',
+          --   },
+        },
+        exact_patterns = {
+          -- Example for a specific filetype with Lua patterns
+          -- Treat patterns.rust as a Lua pattern (i.e "^impl_item$" will
+          -- exactly match "impl_item" only)
+          -- rust = true,
+        },
+
+        -- [!] The options below are exposed but shouldn't require your attention,
+        --     you can safely ignore them.
+
+        separator = nil, -- Separator between context and content. Should be a single character string, like '-'.
+        mode = 'topline',
+      }
+      vim.cmd([[
+        nnoremap [oC :TSContextEnable<cr>
+        nnoremap ]oC :TSContextDisable<cr>
+        nnoremap yoC :TSContextToggle<cr>
+      ]])
       vim.api.nvim_create_autocmd('FileType', {
         group = vim.api.nvim_create_augroup("nvim_init_treesitter", {}),
         pattern = "lua",
