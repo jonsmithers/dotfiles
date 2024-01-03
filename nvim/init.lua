851869d18fcb5e3a9a0a3178c18fa55e91f03132
Jon Smithers <jon@smithers.dev>
2024-01-03 12:02:39 -0500

    nvim: attempt to configure fancy fold column
    This feels fragile.

--- nvim/init.lua
+++ nvim/init.lua
@@ -308,6 +308,45 @@ require('lazy').setup({
     },
   },
 
+  { 'kevinhwang91/nvim-ufo',
+    enabled = os.getenv('NEOVIM_FOLD_COLUMN') == '1',
+    dependencies = {
+      'kevinhwang91/promise-async',
+      { 'luukvbaal/statuscol.nvim',
+        config = function()
+          local builtin = require("statuscol.builtin")
+          require("statuscol").setup({
+            relculright = true,
+            segments = {
+              {text = {builtin.foldfunc}, click = "v:lua.ScFa"},
+              {text = {"%s"}, click = "v:lua.ScSa"},
+              {text = {builtin.lnumfunc, " "}, click = "v:lua.ScLa"}
+            }
+          })
+        end
+      }
+    },
+    opts = {
+      ---@diagnostic disable-next-line: unused-local
+      provider_selector = function(bufnr, filetype, buftype)
+        return {'treesitter', 'indent'}
+      end,
+    },
+    init = function()
+      vim.opt.fillchars:append { foldclose="›", foldopen="⌄", foldsep=" " }
+      vim.o.foldcolumn = '1' -- '0' is not bad
+      vim.o.foldlevel = 99 -- Using ufo provider need a large value, feel free to decrease the value
+      vim.o.foldlevelstart = 99
+      vim.o.foldenable = true
+
+      -- Using ufo provider need remap `zR` and `zM`. If Neovim is 0.6.1, remap yourself
+      vim.keymap.set('n', 'zR', require('ufo').openAllFolds)
+      vim.keymap.set('n', 'zM', require('ufo').closeAllFolds)
+      vim.keymap.set('n', 'zr', require('ufo').openFoldsExceptKinds)
+      vim.keymap.set('n', 'zm', require('ufo').closeFoldsWith) -- closeAllFolds == closeFoldsWith(0)
+    end
+  },
+
   { 'kyazdani42/nvim-tree.lua',
     -- tree buffer mappings
     --   P     | jump to parent
