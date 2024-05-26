52dbe5b57e3fe5eeee7f08235aa96d5d59803316
Jon Smithers <jon@smithers.dev>
2024-05-26 15:55:38 -0400

    nvim: auto-insert ( after selecting function
    Stolen from https://github.com/nvim-lua/kickstart.nvim/blob/master/lua/kickstart/plugins/autopairs.lua

--- nvim/init.lua
+++ nvim/init.lua
@@ -1263,7 +1263,14 @@ require('lazy').setup({
   },
 
   { 'windwp/nvim-autopairs',
-    config = true
+    dependencies = { 'hrsh7th/nvim-cmp' },
+    config = function()
+      require('nvim-autopairs').setup {}
+      -- automatically add `(` after selecting a function or method
+      local cmp_autopairs = require 'nvim-autopairs.completion.cmp'
+      local cmp = require 'cmp'
+      cmp.event:on('confirm_done', cmp_autopairs.on_confirm_done())
+    end,
   },
 })
 
