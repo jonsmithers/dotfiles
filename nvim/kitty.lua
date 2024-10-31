8947239f37538c9e372a09191902d104fb1c9162
Jon Smithers <jon@smithers.dev>
2024-10-31 15:19:35 -0400

    nvim: use different biases for different kitty windows

--- nvim/kitty.lua
+++ nvim/kitty.lua
@@ -56,14 +56,14 @@ function kitty.get_and_focus_window(opts)
   if (global_opts.single_term_mode or opts.persistent_shell) then
     if (global_state.window_id_of_persistent_shell == nil or not kitty.window_exists(global_state.window_id_of_persistent_shell)) then
       -- if (last_terminal == nil or 0 ~= os.execute("kitty @ ls | jq '.[].tabs.[].windows.[].title' | grep --quiet 🏃")) then
-      vim.system({'kitty', '@', 'launch', '--cwd', vim.fn.getcwd(), '--location', 'hsplit'}):wait() -- , '--title', '  '})
+      vim.system({'kitty', '@', 'launch', '--cwd', vim.fn.getcwd(), '--location', 'hsplit', '--bias=40'}):wait() -- , '--title', '  '})
       global_state.window_id_of_persistent_shell = kitty.get_current_window_id()
     else
       vim.system({'kitty', '@', 'focus-window', '--match', 'id:'..global_state.window_id_of_persistent_shell}):wait()
     end
     return global_state.window_id_of_persistent_shell
   else
-    vim.system({'kitty', '@', 'launch', '--cwd', vim.fn.getcwd(), '--location', 'hsplit'}):wait() -- , '--title', '🏃'})
+    vim.system({'kitty', '@', 'launch', '--cwd', vim.fn.getcwd(), '--location', 'hsplit', '--bias=10'}):wait() -- , '--title', '🏃'})
     return kitty.get_current_window_id()
   end
 end
