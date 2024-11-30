662a3b789bfa6ea1fb99b68a25bfd0c8308083bd
Jon Smithers <jon@smithers.dev>
2024-11-30 13:32:25 -0500

    Add snacks

--- nvim/init.lua
+++ nvim/init.lua
@@ -98,6 +98,21 @@ require('lazy').setup({
     },
   },
 
+  { "folke/snacks.nvim",
+    priority = 1000,
+    lazy = false,
+    opts = {
+      -- your configuration comes here
+      -- or leave it empty to use the default settings
+      -- refer to the configuration section below
+      bigfile = { enabled = true },
+      notifier = { enabled = true },
+      quickfile = { enabled = true },
+      statuscolumn = { enabled = false },
+      words = { enabled = true },
+    },
+  },
+
   { 'folke/trouble.nvim',
     --[[
       | P | toggle preview           |
