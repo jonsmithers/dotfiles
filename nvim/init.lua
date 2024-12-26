f39d6a69cc0f658916b0b6ae9dfc74f12b284a5c
Jon Smithers <jon@smithers.dev>
2024-12-26 14:09:12 -0500

    nvim: show macro recording in status line

--- nvim/init.lua
+++ nvim/init.lua
@@ -786,30 +786,41 @@ require('lazy').setup({
     dependencies = dev_icons_enabled and {
       'nvim-tree/nvim-web-devicons'
     } or {},
-    opts = {
-      extensions = {
-        'fugitive',
-        'fzf',
-        'lazy',
-        'nvim-tree',
-        'oil',
-        'quickfix',
-        'trouble',
-      },
-      options = {
-        -- component_separators = { left = '', right = '' },
-        component_separators = { left = '', right = '' },
-        section_separators = { left = '', right = '' },
-      },
-      sections = {
-        lualine_a = {'mode'},
-        lualine_b = {'branch', 'diff', 'diagnostics'},
-        lualine_c = {'filename'},
-        lualine_x = {{ 'searchcount', maxcount=999, timeout=500 }, 'encoding', 'fileformat', 'filetype', },
-        lualine_y = {'progress'},
-        lualine_z = {'location'}
-      },
-    }
+    opts = function()
+
+      local macro_recording = function()
+        local reg_recording = vim.fn.reg_recording();
+        if (reg_recording == '') then
+          return ''
+        end
+        return "RECORDING "..reg_recording
+      end
+
+      return {
+        extensions = {
+          'fugitive',
+          'fzf',
+          'lazy',
+          'nvim-tree',
+          'oil',
+          'quickfix',
+          'trouble',
+        },
+        options = {
+          -- component_separators = { left = '', right = '' },
+          component_separators = { left = '', right = '' },
+          section_separators = { left = '', right = '' },
+        },
+        sections = {
+          lualine_a = { 'mode', macro_recording },
+          lualine_b = {'branch', 'diff', 'diagnostics'},
+          lualine_c = {'filename'},
+          lualine_x = {{ 'searchcount', maxcount=999, timeout=500 }, 'encoding', 'fileformat', 'filetype', },
+          lualine_y = {'progress'},
+          lualine_z = {'location'}
+        },
+      }
+    end,
   },
 
   { 'nvim-treesitter/nvim-treesitter',
